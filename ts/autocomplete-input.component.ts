a513d9a11ca2685f14ed60b25d8aa3e358ecb9c1
Thiago Leite <thiagofmleite@gmail.com>
2023-08-28 12:52:22 -0300

    Update autocomplete-input.component.ts

--- ts/autocomplete-input.component.ts
+++ ts/autocomplete-input.component.ts
@@ -1,59 +1,58 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output, ElementRef, ViewChild } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { pipe, } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-
+import { fromEvent } from 'rxjs';
+import {
+  filter,
+  map,
+  debounceTime,
+  distinctUntilChanged,
+} from 'rxjs/operators';
 
 @Component({
   selector: "autocomplete-input",
   template: `
     <div class="wrapper">
       <div class="control" [ngClass]="{ 'is-loading': isLoading }">
-        <input type="text" class="input" (input)="onInput($event.target.value)" />
+        <input #input type="text" class="input" />
       </div>
       <div class="list is-hoverable" *ngIf="list.length > 0">
-        <a class="list-item" *ngFor="let item of list" (click)="select(item)">{{ item }}</a>
+        <a (click)="selectItem(item)" class="list-item" *ngFor="let item of list">{{ item }}</a>
       </div>
     </div>
   `,
 })
 export class AutocompleteComponent {
+  @ViewChild('input', { static: true }) inputSearch!: ElementRef;
   @Output() public onSelect = new EventEmitter();
-  
+  isLoading: boolean = false;
   list: string[] = [];
-  isLoading = false;  
 
   constructor(private http: HttpClient) {}
-  ngOnInit() {  
+  ngOnInit() {
+    fromEvent(this.inputSearch.nativeElement, 'keyup')
+    .pipe(
+      map((event: any) => {
+        const value: string = event.target.value;
+        return value;
+      }),      
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe((query) => {
+      this.isLoading = true;
 
+      this.fetchData(query).subscribe((items) => {
+        this.list = items;
+        this.isLoading = false;
+      })
+    })
   }
 
-  ngAfterViewInit() {
-
-  }
-
-  fetchData(query: string) {
+  fetchData(query: string) {    
     const apiUrl = `https://example.com/api/items?q=${query}`;
     return this.http.get<string[]>(apiUrl);
   }
 
-  onInput(query: string) {
-    if(!query) {
-      this.list = [];
-      return;
-    }
-
-    this.isLoading = true;
-    this.fetchData(query)
-    .pipe(debounceTime(500))
-    .subscribe((items) => {
-      this.list = items;
-      this.isLoading = false;
-    });
-  }
-
-  select(item: string) {
+  selectItem(item: string) {
     this.onSelect.emit(item);
   }
 }
-
